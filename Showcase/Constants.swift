3ce1db6857975ce69e50cecf43b1582fbeaf701a
Tbakhi <Tbakhi@Tbakhis-iMac.local>
2016-03-20 21:34:11 -0500

    created project and set up login screen

---
+++ Showcase/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Showcase
+//
+//  Created by Tbakhi on 3/20/16.
+//  Copyright © 2016 Tbakhi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR:CGFloat = 157.0 / 255.0
+
